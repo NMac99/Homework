ec742c0c5c813a06e3827ed6fbcbe3dadd10ec4d
narekarsenyan <narekarsenyan99@gmail.com>
2022-04-24 23:34:06 +0400

    REMOVE folder

--- Narek Arsenyan/Task5.py
+++
@@ -1,9 +0,0 @@
-num = input("set number: ")
-
-def createTuple(a = num):
-    word = str(a)
-    digits = ()
-    for n in word:
-        digits += (int(n),)
-    return digits
-print(createTuple())
